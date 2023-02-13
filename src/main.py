de33e44e9b01c6c25297c4d11d4dbf01c3b2a7d6
Alejandro <alejab3@uci.edu>
2023-02-13 11:18:49 -0800

    parse start

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import os
 import Document
-
+from bs4 import BeautifulSoup
+import json
 def parse(file, id: int) -> Document:
-    #weightDict = loop through all the {key:word : val: int}
+    f = json.loads(file)
+    soup = BeautifulSoup(file["content"])
+    WeightDict = {word : 1 for word in soup.title.string}
     #tfFreqDict = loop through keys of weightDict and create dict {key:stemWord : val: (freq:float, weight:int)}
     # instantiate Document -> Document(id, tfFreqDict)
     # return Document 
+    print(WeightDict)
     pass
 
 
@@ -13,14 +17,16 @@ def main():
 
     # assign directory
     directory = 'SearchEngine/DEV/'
-    
     # iterate over files in
     # that directory
     for subdir, dirs, files in os.walk(directory):
         for file in files:
             file =os.path.join(subdir, file)
             with open(file, 'r') as opened:
+                parse(file)
                 print('success')
+                break
+
 
     
 
