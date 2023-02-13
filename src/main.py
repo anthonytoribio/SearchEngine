ce697c609476bdbca343102f573c8191baecf48d
Anthony Toribio <anthonytoribio01@gmail.com>
2023-02-13 10:54:44 -0800

    Added parse psuedocode

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import Document
+
+def parse(file, id: int) -> Document:
+    #weightDict = loop through all the {key:word : val: int}
+    #tfFreqDict = loop through keys of weightDict and create dict {key:stemWord : val: (freq:float, weight:int)}
+    # instantiate Document -> Document(id, tfFreqDict)
+    # return Document 
+    pass
