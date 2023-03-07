517a62666a01b2cc085c276d6540157b76a6ec31
Alejandro <alejab3@uci.edu>
2023-03-06 19:16:42 -0800

    getting post from client to server.py

--- server/src/server.py
+++ server/src/server.py
@@ -1,16 +1,31 @@
-from flask import Flask
-
+from flask import Flask, request, redirect, url_for
+from main import FILE, main
+import json
 app = Flask(__name__)
 @app.route("/")
-def test1():
-    return 'anthony'
+def start():
+    return 'works'
 
 @app.route("/test")
+
 def test():
     return {
-        "name":"hey",
+        "name":"h",
         "bro":"nothing"
     }
 
+@app.route("/test/help", methods=["POST", "GET"])
+def get_query():
+    if request.method == "POST":
+        return_lst = []
+        query = request.json["state"]
+        # results = main(query)
+        results = ["hamburger.uci.ics.edu", "example.com", query+".com"]
+        for result in results:
+            return_lst.append({"url":result,"description":"NULL"})
+        return return_lst
+    else:
+        return
+    
 if __name__=='__main__':
     app.run(debug=True, port=4999)
